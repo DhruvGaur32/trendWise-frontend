34de2411c047f9e2088d6d94a02c921e33a41d9f
DhruvGaur32 <154977139+DhruvGaur32@users.noreply.github.com>
2025-07-02 05:45:58 +0530

    change for deployment

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -3,8 +3,8 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
-import User from "@/models/User";
-import connectDB from "@/lib/db";
+import User from "../../../models/User"
+import connectDB from "../../../lib/db";
 
 export const authOptions = {
     providers: [
